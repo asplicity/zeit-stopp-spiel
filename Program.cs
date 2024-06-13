15aa58fec4d712a7bbb44eaa9777fd78af0d416d
asplicity <ticobeier@gmail.com>
2024-06-13 17:25:27 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Drücke 2 mal eine Taste so schnell hintereinander wie du kannst");
+        Console.ReadKey();
+        var start = DateTime.Now;
+        Console.ReadKey();
+        var end = DateTime.Now;
+        var time = end - start;
+        Console.WriteLine($"\nDu brauchtest {time.TotalSeconds} Sekunden");
+    }
+}
